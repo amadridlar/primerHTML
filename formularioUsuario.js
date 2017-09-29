cbecba034854df05bc53b07964ddc499578d8523
amadrid <amadridlar@gmail.com>
2017-09-29 11:53:29 +0200

    Actualizado formularioUsuario. Añadido seguromedico.html

--- formularioUsuario.js
+++ formularioUsuario.js
@@ -1,10 +1,28 @@
 var app = angular.module('myApp', []);
-app.controller('controller', function($scope){
+
+app.controller('controller',['$scope',  function($scope){
+	$scope.userData = {
+		name:'',
+		lastName:'',
+		nif:'',
+		children: false,
+		childrenNumber: 0,
+		married: false,
+		numberOfMates: 0,
+		item:'',
+	}
+
 	$scope.acceptData = function (){
-		if($scope.inputName.$valid){
+		if($scope.myTable.inputName.$valid && $scope.myTable.inputLastName.$valid  && $scope.myTable.inputEmail.$valid){
 		document.getElementById('userForm').style.display = 'block';
 		}
 	}
-	
 
-});
+	$scope.addItem = function(){
+	$scope.itemCopy = $scope.userData.item;
+	console.log($scope.itemCopy);
+	};
+
+
+}]);
+
